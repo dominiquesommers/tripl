298960a3e6ccb5d5373cff356df09ca190711fa0
Dominique Sommers <dominiquesommers@Mac.home>
2025-12-22 20:15:26 +0100

    Fix for gh pages.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'trip/:tripId',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'trip/:tripId/:planId',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
