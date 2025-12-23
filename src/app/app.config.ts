449022ad7eb01edc17ead912fb582360d890412a
Dominique Sommers <dominiquesommers@Mac.home>
2025-12-23 08:14:50 +0100

    Fix gh pages redirect.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withRouterConfig, withHashLocation } from '@angular/router';
-import { APP_BASE_HREF } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -8,7 +7,6 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    { provide: APP_BASE_HREF, useValue: '/tripl/' },
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes,
       withComponentInputBinding(),
