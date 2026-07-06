08bd9e35f1fb8408ce088114f478c969f62656ef
Dominique Sommers <dominiquesommers@192.168.1.5>
2026-07-06 14:15:55 +0700

    Fix ofscreen popup bug.

--- src/app/services/popup.ts
+++ src/app/services/popup.ts
@@ -44,17 +44,45 @@ export class PopupService {
     // Position on body
     const el = ref.location.nativeElement as HTMLElement;
     el.style.position = 'fixed';
-    el.style.top = config.position.top + 'px';
-    el.style.left = config.position.left + 'px';
-    el.style.transform = 'translateX(-50%)';
     el.style.zIndex = '9999';
     el.setAttribute('data-popup', '');
-    console.log(config.position);
-    console.log(el.style.left, el.style.top);
 
     this.appRef.attachView(ref.hostView);
     document.body.appendChild(el);
     this.ref = ref;
+
+    const padding = 8;
+    const vw = window.innerWidth;
+    const vh = window.innerHeight;
+
+    const popupWidth = el.offsetWidth;
+    const popupHeight = el.offsetHeight;
+    let targetLeft = config.position.left - (popupWidth / 2);
+    let targetTop = config.position.top;
+    // Constrain Left and Right edges
+    if (targetLeft < padding) {
+      targetLeft = padding; // Clamp to left screen edge
+    } else if (targetLeft + popupWidth > vw - padding) {
+      targetLeft = vw - popupWidth - padding; // Clamp to right screen edge
+    }
+
+    // Constrain Top and Bottom edges
+    if (targetTop < padding) {
+      targetTop = padding; // Clamp to top screen edge
+    } else if (targetTop + popupHeight > vh - padding) {
+      targetTop = vh - popupHeight - padding; // Clamp to bottom screen edge
+    }
+
+    el.style.left = `${targetLeft}px`;
+    el.style.top = `${targetTop}px`;
+    el.style.transform = 'none';
+
+    // el.style.top = config.position.top + 'px';
+    // el.style.left = config.position.left + 'px';
+    // el.style.transform = 'translateX(-50%)';
+
+    // console.log(config.position);
+    // console.log(el.style.left, el.style.top);
 
     return ref as ComponentRef<T>;
   }
